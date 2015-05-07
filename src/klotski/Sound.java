361ff13c3a996d9871837969432ab2ba397b2151
michael <michael@10.0.7.134>
2015-05-06 17:18:52 -0700

    added a winner sound. will continue to work on sound effects. added a sound effect for movement of blocks. whenever you see michael as an author that is me. -joe

--- src/klotski/Sound.java
+++ src/klotski/Sound.java
@@ -5,4 +5,7 @@ import java.applet.AudioClip;
 public class Sound {
     public static final AudioClip winner = 
             Applet.newAudioClip(Sound.class.getResource("winner.wav"));
+    
+    public static final AudioClip move = 
+            Applet.newAudioClip(Sound.class.getResource("move.wav"));
 }
