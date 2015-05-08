8d67474d03838cc3e503d11d42493c24b6e41e64
Ravinder <Ravinder@10.0.0.4>
2015-05-08 15:23:39 -0700

    My block

--- src/klotski/BlockList.java
+++ src/klotski/BlockList.java
@@ -5,10 +5,10 @@ import java.awt.Rectangle;
 class BlockList {
   static Block[] blocks = new Block[] {
     new Block(1, 0, 2, 2, "joeWoodPc"),
-    new Block(0, 0, 1, 2, "1by2"),
-    new Block(0, 2, 1, 2, "1by2"),
-    new Block(3, 0, 1, 2, "1by2"),
-    new Block(3, 2, 1, 2, "1by2"),
+    new Block(0, 0, 1, 2, "Ravuth1by2"),
+    new Block(0, 2, 1, 2, "Ravuth1by2"),
+    new Block(3, 0, 1, 2, "Ravuth1by2"),
+    new Block(3, 2, 1, 2, "Ravuth1by2"),
     new Block(1, 2, 2, 1, "2by1"),
     new Block(1, 3, 1, 1, "1by1"),
     new Block(2, 3, 1, 1, "1by1"),
